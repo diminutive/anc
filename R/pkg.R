39dfc68deddd01d48615ce2b865f13b8f5c7daf0
Michael Sumner <mdsumner@gmail.com>
2014-12-16 02:35:07 -1000

    using roxygen requires the little @useDynLib directive, say in the pkg.R file

---
+++ R/pkg.R
@@ -0,0 +1,28 @@
+#
+# Copyright Michael D. Sumner
+#
+
+#' @useDynLib anc
+
+#' @name anc-package
+#' @aliases anc-package, anc
+#' @docType package
+#' @title R tools for NetCDF.  
+#' @description
+#' NetCDF test
+#' @details
+#' \tabular{ll}{
+#' Package: \tab anc\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0-1\cr
+#' Date: \tab 2014-12-16\cr
+#' License: \tab GPL-3 \cr
+#' }
+#' Access to NetCDF. 
+#' @author
+#' Michael D. Sumner \cr \url{http://staff.acecrc.org.au/~mdsumner} \cr \cr
+#' Maintainer: Michael D. Sumner \email{mdsumner@@gmail.com} \cr
+#' @references \url{https://github.com/mdsumner}
+#' @keywords package
+#' @import Rcpp
+NULL
